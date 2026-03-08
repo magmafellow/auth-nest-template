6e88801739fafc09e7fe92c33a17886cd215bfff
Alexey|Ubuntu <magmafellow@gmail.com>
2026-03-08 14:27:06 +0300

    Pipes tutorial learned

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from "./prisma.service.js"; 
 import { UserService } from "./user.service.js"; 
 import { UserModule } from './user/user.module.js';
+import { CatModule } from './cat/cat.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, CatModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, UserService],
 })
